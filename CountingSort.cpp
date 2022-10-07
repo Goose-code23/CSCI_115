d0f503039901933c1ab6846b8eaafe8ebeeb09d9
Gustavo Lita <76187202+Goose-code23@users.noreply.github.com>
2022-10-06 22:14:36 -0700

    Update CountingSort.cpp

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -4,59 +4,61 @@
 #include<chrono>
 using namespace std;
 
-void PrintArray(int array[], int size){
-    for (int i = 0; i < size; i ++){
+void PrintArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
         cout << array[i] << " ";
-        cout << endl;
     }
 };
 
-void countSort(int array[], int size){
-    
+void countSort(int array[], int size) {
+
     int out[10];
     int count[10];
     int maximum = array[0];
 
-    for (int i = 1; i < size; i++){
-        if (array[i] > maximum){
+    for (int i = 1; i < size; i++) {
+        if (array[i] > maximum) {
             maximum = array[i];
         }
     }
 
-    for (int j = 0; j <= maximum; j++){
+    for (int j = 0; j <= maximum; j++) {
         count[j] = 0;
     }
 
-    for(int k = 0; k < size; k++){
-        count[array[k]++];
+    for (int k = 0; k < size; k++) {
+        count[array[k]]++;
     }
 
-    for (int l = 1; l <= maximum; l++){
+    for (int l = 1; l <= maximum; l++) {
         count[l] += count[l - 1];
     }
 
-    for (int m = size - 1; m >= 0; m--){
+    for (int m = size - 1; m >= 0; m--) {
         out[count[array[m]] - 1] = array[m];
         count[array[m]]--;
     }
 
-    for (int n = 0; n < size; n++){
+    for (int n = 0; n < size; n++) {
         array[n] = out[n];
     }
 }
 
-int main(){
-    int array1[] = {2, 0, 1, 0};
+int main() {
+    int a[] = { 1,5,6,7,9,5,3,7,8 };
 
-    int z = sizeof(array1)/ sizeof(array1[0]);
+    int n = sizeof(a) / sizeof(a[0]);
 
-    auto start1 = chrono::high_resolution_clock::now();
-
-    countSort(array1, z);
-    PrintArray(array1, z);
-    auto end1 = chrono::high_resolution_clock::now();
-    cout << "     Time for Counting Sort: ";
-	double time_taken1 = chrono::duration_cast<chrono::nanoseconds>(end1 - start1).count();
-	time_taken1 *= 1e-9;
-	cout << time_taken1 << endl;
+    cout << "Before sorting: ";
+    PrintArray(a, n);
+    cout << endl;
+    auto start1 = std::chrono::steady_clock::now();
+    cout << "After sorting:";
+    countSort(a, n);
+    PrintArray(a, n); 
+    cout << endl;
+    auto end1 = std::chrono::steady_clock::now();
+    std::chrono::duration<double>seconds1 = end1 - start1;
+    cout << "Time for Counting Sort: ";
+    std::cout << seconds1.count() << "s\n";
 }
