a07935e21f55c2e4f9fbec7b5cd1cadbad8e524d
Gustavo Lita <76187202+Goose-code23@users.noreply.github.com>
2022-11-15 14:47:53 -0800

    Update Dynamic-programming.cpp

--- Dynamic-programming.cpp
+++ Dynamic-programming.cpp
@@ -1,46 +1,51 @@
 #include<iostream>
-#include<iomanip>
 #include<chrono>
+#define MAX 100
 using namespace std;
 
-void print_opt_parens(int s[][10], int i, int j)
-{
-    if (i == j)
-        cout << "A" << i;
-    else
-    {
-        cout << "(";
-        print_opt_parens(s, i, s[i][j]);
-        print_opt_parens(s, s[i][j] + 1, j);
-        cout << ")";
-    }
+void print_opt_paren(int s[][MAX], int i, int j){
+	if (i == j)
+		cout << "A" << i;
+	else {
+		cout << "(";
+		print_opt_paren(s, i, s[i][j]);
+		print_opt_paren(s, s[i][j] + 1, j);
+		cout << ")";
+	}
 }
 
-void matrix_chain_order(int p[], int n)
-{
-    int m[10][10], s[10][10];
-    int i, j, k, L, q;
-    for (i = 1; i < n; i++)
-        m[i][i] = 0;
-    for (L = 2; L < n; L++)
-    {
-        for (i = 1; i < n - L + 1; i++)
-        {
-            j = i + L - 1;
-            m[i][j] = INT_MAX;
-            for (k = i; k <= j - 1; k++)
-            {
-                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                if (q < m[i][j])
-                {
-                    m[i][j] = q;
-                    s[i][j] = k;
-                }
-            }
-        }
-    }
-    cout << "Optimal Parenthesization is : ";
-    print_opt_parens(s, 1, n - 1);
-    cout << endl;
-    cout << "Optimal Cost is : " << m[1][n - 1];
+void matrix_chain_order(int p[], int n) {
+	int m[MAX][MAX], s[MAX][MAX];
+
+	for (int i = 1; i <= n; i++)
+		m[i][i] = 0;
+
+	for (int l = 2; l <= n; l++) {
+		for (int i = 1; i <= n - l + 1; i++) {
+			int j = i + l - 1;
+			m[i][j] = INT_MAX;
+			for (int k = i; k < j; k++) {
+				int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+				if (q < m[i][j]) {
+					m[i][j] = q;
+					s[i][j] = k;
+				}
+			}
+		}
+	}
+
+	cout << "\nOptimal Cost is : " << m[1][n];
+	print_opt_paren(s, 1, n);
+}
+
+int main() {
+	int p[] = { 10, 20, 30, 40, 30 };
+	int n = (sizeof(p) / sizeof(p[0])) - 1;
+	matrix_chain_order(p, n);
+
+	int p2[] = { 4, 10, 3, 12, 20, 7 };
+	int z = (sizeof(p2) / sizeof(p2[0])) - 1;
+	matrix_chain_order(p2, z);
+
+	return 0;
 }
